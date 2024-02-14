c8fcda110e2b7bb08bd434bf914645a2081b6664
VIMALCHANDAR P K <vimalchandar2003@gmail.com>
2024-02-14 09:05:03 +0530

    corserr

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,35 @@
-import { useEffect, useState } from "react";
-import ExpenseForm from "./Components/ExpenseForm";
-import ExpenseItem from "./Components/ExpenseItem"
+import { useEffect,useState } from "react";
+import ExpenseForm from "./components/ExpenseForm";
+import ExpenseItem from "./components/ExpenseItem"
 import axios from "axios";
 
-
-
 const App =()=>{
-
-    const [expenses, setExpenses] = useState([]  )
+  const [expenses, setExpenses] = useState([])
 
   useEffect(()=>{
-    axios.get('https://expense-tracker-api-8bby.onrender.com/get-entry')
+    axios.get('https://expensetracker-api2.onrender.com/entry')
   .then(res=>{
     console.log(res.data)
     setExpenses(res.data)
   })
-  .catch(er=>console.log(err))
+  .catch(err=>console.log(err))
+  },[])
+  const addExpense =(title,amount)=>{
+    const nextId=expenses[expenses.length-1].id+1
+    setExpenses([...expenses,{id:nextId ,title:title,amount:amount}])
+  }
+  const deleteExpense =(id)=>{
+    setExpenses(expenses.filter((exp)=>exp.id !== id))
+  }
+
+  let income=0,expense=0;
+  expenses.forEach((exp)=>{
+    if(exp.amount>0){
+      income += exp.amount
+    }else{
+      expense -= exp.amount
+    }
   })
-    const addExpense = (title, amount) => {
-      const nextId = expenses[expenses.length - 1].id + 1
-      setExpenses([...expenses, {title:title, amount:amount}])
-    }
- const deleteExpense = (id) => {
-  setExpenses(expenses.filter((exp) => exp.id!== id))
-
-
-
- }
-  let income = 0;
-  let expense = 0;
-  expenses.forEach((exp) => {
-    if (exp.amount > 0) {
-      income += exp.amount;
-    } else {
-      expense -= exp.amount;
-    }
-  });
   return(
     <>
     <div> 
